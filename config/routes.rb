ee46d317e02ffa433fc24f20856c05ef75c746d6
okaybody <taesuk.hwang@gmail.com>
2015-03-29 14:39:17 +0000

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Mvc::Application.routes.draw do
+	get 'home/hello' => 'home#hello'
   # The priority is based upon order of creation: first created -> highest priority.
+
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
